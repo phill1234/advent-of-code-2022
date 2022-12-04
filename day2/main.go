93014205e42c7f3d56118325a26ed087b08bc15b
Philipp Dienst <philipp@entwicklerheld.de>
2022-12-04 20:06:36 +0100

    - fixed game logic

--- day2/main.go
+++ day2/main.go
@@ -80,11 +80,11 @@ func WhatToSelect(opponentSelection string, result string) string {
 	// X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
 	if result == "X" {
 		if opponentSelection == "X" {
-			return "Y"
+			return "Z"
 		} else if opponentSelection == "Y" {
 			return "X"
 		} else {
-			return ""
+			return "Y"
 		}
 	} else if result == "Y" {
 		if opponentSelection == "X" {
