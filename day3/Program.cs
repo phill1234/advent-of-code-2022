e339a4b0f58116cd54da40dd22aa06184a246e03
pdienst <philipp@entwicklerheld.de>
2022-12-07 21:11:09 +0100

    - implemented part 2 without completion :D

--- day3/Program.cs
+++ day3/Program.cs
@@ -4,6 +4,37 @@
     {
         static void Main(string[] args)
         {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            char[] letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            int sum = 0;
+            // get actual path
+            string path = Directory.GetCurrentDirectory();
+            // get path to input file
+            string inputPath = Path.Combine(path, "input.txt");
+
+            foreach (string line in System.IO.File.ReadLines(inputPath))
+            {
+                //split line into two parts at half of the length
+                string compartments1 = line.Substring(0, line.Length / 2);
+                string compartments2 = line.Substring(line.Length / 2);
+
+                // find duplicates in compartments1 and compartments2
+                string dublicates = new string(compartments1.Intersect(compartments2).ToArray());
+
+                foreach (char c in dublicates)
+                {
+                    sum += Array.IndexOf(letters, c) + 1;
+                }
+            }
+            Console.WriteLine($"Part1: {sum}");
+        }
+        static void Part2()
+        {
             int sum = 0;
             // get actual path
             string path = Directory.GetCurrentDirectory();
@@ -13,35 +44,42 @@
             // all lowercase letters as array
             char[] letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
 
+            // empty string list
+            List<string> entries = new List<string>();
+
+
             foreach (string line in System.IO.File.ReadLines(inputPath))
             {
-               //split line into two parts at half of the length
-                string compartments1 = line.Substring(0, line.Length / 2);
-                string compartments2 = line.Substring(line.Length / 2);
-
-                // get all lower case letters from compartments1
-                string lowerCaseCompartments1 = new string(compartments1.Where(c => char.IsLower(c)).ToArray());
-                string upperCaseCompartments1 = new string(compartments1.Where(c => char.IsUpper(c)).ToArray());
-
-                // get all lower case letters from compartments2
-                string lowerCaseCompartments2 = new string(compartments2.Where(c => char.IsLower(c)).ToArray());
-                string upperCaseCompartments2 = new string(compartments2.Where(c => char.IsUpper(c)).ToArray());
-                
-                // find duplicates in compartments1 and compartments2
-                string duplicatesLowerCase = new string(lowerCaseCompartments1.Intersect(lowerCaseCompartments2).ToArray());
-                string duplicatesUpperCase = new string(upperCaseCompartments1.Intersect(upperCaseCompartments2).ToArray());
-
-                
-                foreach (char c in duplicatesLowerCase)
+                // add line to entries string list
+                entries.Add(line);
+                if (entries.Count == 3)
                 {
-                    sum += Array.IndexOf(letters, c) + 1;
+                    char dublicatOfAllThree = FindDuplicate(entries)[0];
+                    sum += Array.IndexOf(letters, dublicatOfAllThree) + 1;
+                    entries = new List<string>();
                 }
-                foreach (char c in duplicatesUpperCase)
-                {
-                    sum += Array.IndexOf(letters, c) + 1;
-                }                
             }
-            Console.WriteLine(sum);
+            Console.WriteLine($"Part2: {sum}");
+
+        }
+        static string FindDuplicate(List<string> entries)
+        {
+            string[] entriesAsArray = entries.ToArray();
+            string matchingDublicate = "";
+            // find duplicates in compartments1 and compartments2
+            matchingDublicate = new string(entriesAsArray[0].Intersect(entriesAsArray[1]).ToArray());
+
+            if (matchingDublicate == "")
+            {
+                return "";
+            }
+
+            matchingDublicate = new string(matchingDublicate.Intersect(entriesAsArray[2]).ToArray());
+            if (matchingDublicate == "")
+            {
+                return "";
+            }
+            return matchingDublicate;
         }
     }
 }
