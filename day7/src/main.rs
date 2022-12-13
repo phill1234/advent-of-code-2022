a053a5646554175460d5402e39aa161149caa3f3
pdienst <philipp@entwicklerheld.de>
2022-12-13 07:38:00 +0100

    - started to implement day7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -6,18 +6,50 @@ fn main() {
     // read input file line by line
     let input = std::fs::read_to_string("input.txt").unwrap();
     let lines = input.lines();
-    
+
     // map of file lists in directories
-    let mut directories: std::collections::HashMap<&str, Vec<File>> = std::collections::HashMap::new();
-    
-    for mut line in lines {
+    let mut directories: std::collections::HashMap<String, Vec<File>> =
+        std::collections::HashMap::new();
+    let mut actual_dir = "/".to_string();
+
+    for line in lines {
         if line.starts_with("$") {
-            line = line.trim_start_matches("$");
-            run_command(line);
+            let command = line.trim_start_matches("$ ");
+            run_command(command, &mut actual_dir, &mut directories);
         }
     }
+    println!("{}", actual_dir);
 }
-
-fn run_command(command: &str) {
-    println!("Running command: {}", command);
+fn run_command(
+    command: &str,
+    actual_dir: &mut String,
+    directories: &mut std::collections::HashMap<String, Vec<File>>,
+) {
+    let mut splitted_command_line = command.split_whitespace();
+    let command = splitted_command_line.next().unwrap();
+    match command {
+        "cd" => {
+            let dir = splitted_command_line.next().unwrap().to_string();
+            *actual_dir = dir;
+        }
+        "mkdir" => {
+            let dir = splitted_command_line.next().unwrap();
+            // insert file in in new path
+            let file = File { size: 0 };
+            let file_list = vec![file];
+            directories.insert(dir.to_string(), file_list);
+        }
+        "touch" => {
+            let file = splitted_command_line.next().unwrap();
+            let size = splitted_command_line
+                .next()
+                .unwrap()
+                .parse::<u64>()
+                .unwrap();
+            let file = File { size };
+            let dir = splitted_command_line.next().unwrap();
+            directories.get_mut(dir).unwrap().push(file);
+        }
+        _ => {}
+    }
 }
